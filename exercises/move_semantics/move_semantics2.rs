939b29c9af99438c0a00cfab2b5cb64ace4dd51b
Dabao Meng <dabao@macbook-pro-de-dabao.home>
2019-03-09 10:22:35 +0100

    Première série d'exercices

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,9 +2,10 @@
 // Make me compile without changing line 10! Scroll down for hints :)
 
 fn main() {
-    let vec0 = Vec::new();
+    let vec0: Vec<i32> = Vec::new();
+    //let vec2: Vec<i32> = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(&vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -15,14 +16,14 @@ fn main() {
 
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
+    let mut copy = vec.clone();
 
-    vec.push(22);
-    vec.push(44);
-    vec.push(66);
+    copy.push(22);
+    copy.push(44);
+    copy.push(66);
 
-    vec
+    copy
 }
 
 
--- exercises/tests/tests3.rs
+++ exercises/tests/tests3.rs
@@ -13,7 +13,12 @@ mod tests {
 
     #[test]
     fn is_true_when_even() {
-        assert!();
+        assert!(true == is_even(42));
+    }
+
+    #[test]
+    fn is_false_when_odd() {
+        assert!(false == is_even(5));
     }
 }
 
